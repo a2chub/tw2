9337ae74eb53d5fd71cd7c4186309eb70c6953a4
atusi <atusi@atusi-MBP.local>
2009-02-19 02:02:46 +0900

    win version thx mattn

--- tw2.py
+++ tw2.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
 # coding:utf-8
 
-import sys, simplejson, urllib2, urllib, twitter as tw, readline
+import sys, urllib2, urllib, twitter as tw, readline, simplejson, os
 from pit import Pit
 import logging
 
-source = '清楚なクライアント'
+try:
+  import Growl
+  g = Growl.GrowlNotifier(
+    applicationName='Pytter', notifications=['SyntaxError', 'PytterNotify'])
+  g.register()
+except:
+  pass
+
+def _T(text):
+  if 'nt' == os.name:
+    return text.encode('mbcs', 'ignore')
+  else:
+    return text
 
 # KeyConfig
 getFriendsTimeLineKey = ['friends', 'f']
 getRepliesKey = ['res', 'r', '']
 sendPostKey = ['post', 'p']
-exitKey = ['xx', 'ZZ', 'exit', 'bye']
+exitKey = ['x', 'ZZ', 'exit', 'bye']
 
 # Log file Name
-logFileName = './wtLog.log'
-logging.basicConfig(filename=logFileName, level=logging.INFO, format='%(message)s',)
+#logFileName = './wtLog.log'
+#logging.basicConfig(filename=logFileName, level=logging.INFO, format='%(message)s',)
 
 class Wassr:
   def __init__(self, user, passwd):
@@ -24,7 +36,7 @@ class Wassr:
   def post(self, text):
     text = text.encode('utf-8')
    #text = text.encode('utf-7')
-    self.getOpener().open('http://api.wassr.jp/statuses/update.json', urllib.urlencode({'status':text,'source':source}))
+    self.getOpener().open('http://api.wassr.jp/statuses/update.json', urllib.urlencode({'status':text}))
   def getTimeline(self):
     url = 'http://api.wassr.jp/statuses/friends_timeline.json'
     r = self.getOpener().open(url)
@@ -44,19 +56,6 @@ class Wassr:
     return opener
 
 
-class Outputz:
-  def __init__(self,key,uri):
-    self.key = key
-    self.uri = uri
-  def post(self,count):
-    param = urllib.urlencode({'key':self.key,'uri':self.uri, 'size':count})
-    self.getOpener().open('http://outputz.com/api/post', param)
-  def getOpener(self):
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-agent', 'OutputsMiniblogClient(%s)' % self.uri)]
-    return opener
-
-
 if __name__ == "__main__":
   import os 
   readline.parse_and_bind("tab: complete")
@@ -65,10 +64,6 @@ if __name__ == "__main__":
   wassr = Wassr(wassr_config['user'],wassr_config['password'])
   twitter_config = Pit.get('twitter.com',{'require' : {'user':'Your twitter name','password':'Your twitter password'}})
   twitter = tw.Api(twitter_config['user'], twitter_config['password'])
-  # outpuz setting
-  outputz_key = Pit.get('outputz.com',{'require' : {'key':'Your outputz key'}})['key']
-  twitter_outputz = Outputz(outputz_key, 'http://twitter.com/%s' % twitter_config['user'])
-  wassr_outputz = Outputz(outputz_key, 'http://wassr.jp/user/%s' % wassr_config['user'])
 
   friends = set()
   friendsTimeLine = []
@@ -82,21 +77,21 @@ if __name__ == "__main__":
     twit = ''.join(input[1:])
     wassr.post(input)
     twitter.PostUpdate(input)
-    twitter_outputz.post(len(input))
-    wassr_outputz.post(len(input))
     print 'update : ' + input
 
   def getReplies(wassr, twitter):
     # get twitter friends Replies
     print '\twassr replies\t'
     for data in reversed(wassr.getReplies()):
-      print '%-12s : %s' % (data['user_login_id'] ,data['text'])
+      #print '%-12s : %s' % (data['user_login_id'] ,data['text'])
+      print _T('%-12s : %s' % (data['user_login_id'] ,data['text']))
       friends.add(data['user_login_id'])
   
     # get twitter friends Replies        
-    print '\n\twassr replies\t'
+    print '\n\ttwitter replies\t'
     for data in reversed(twitter.GetReplies()):
-      print '%-12s : %s' % (data.GetUser().GetScreenName() ,data.GetText())
+      #print '%-12s : %s' % (data.GetUser().GetScreenName() ,data.GetText())
+      print _T('%-12s : %s' % (data.GetUser().GetScreenName() ,data.GetText()))
       friends.add(data.GetUser().GetScreenName())
     return 0
 
@@ -105,7 +100,8 @@ if __name__ == "__main__":
     # Get FriendsTimeLine
     print ' -----  wassr Friends Time Line  -----'
     for data in reversed(wassr. getTimeline()):
-      print "%-12s: %s" % (data['user_login_id'] ,data['text'])
+      #print "%-12s: %s" % (data['user_login_id'] ,data['text'])
+      print _T("%-12s: %s" % (data['user_login_id'] ,data['text']))
       twit = "wr[]%-12s: %s" % (data['user_login_id'] ,data['text'])
       if twit in friendsTimeLine:
         pass
@@ -114,37 +110,51 @@ if __name__ == "__main__":
         friendsTimeLine.append(twit)
     print '\n  -----  twitter Friends Time Line  -----'
     for data in reversed(twitter.GetFriendsTimeline()):
-      print '%-12s : %s' % (data.GetUser().GetScreenName() ,data.GetText())
+      #print '%-12s : %s' % (data.GetUser().GetScreenName() ,data.GetText())
+      print _T('%-12s : %s' % (data.GetUser().GetScreenName() ,data.GetText()))
       # append log
       twit  = "tw[%s]%-12s: %s" % (data.GetCreatedAt(), data.GetUser().GetScreenName(), data.GetText())
       if twit in friendsTimeLine:
         pass
       else:
         logging.info(twit)
+        
+        '''
+        g.notify(
+          noteType='PytterNotify', 
+          title='%s'%(data.GetUser().GetScreenName()), 
+          description='%s'%(data.GetText()), 
+          sticky=False)
+        '''
+
         friendsTimeLine.append(twit)
  
   readline.set_completer(complete)
 
-  prompt = '\n cmd: Friendstimeline[f] eXit[xx] \n> '
+  prompt = '\n cmd: Friendstimeline[f] eXit[x] \n> '
+  
   while True:
-    input = raw_input(prompt).split(" ")
+    if os.name == 'nt':
+      print prompt,
+      input = sys.stdin.readline().decode('mbcs').encode('utf-8').strip().split(" ")
+    else:
+      all_input = raw_input(prompt)
+      input = all_input.split(" ")
     if input[0] != '':
-      if len(input) == 1:
-        if len(input[0]) > 2:
-          twitPost(unicode(' '.join(input), 'utf-8'))
-          continue
-        else:
-          if input[0] in getFriendsTimeLineKey: 
-            getFriensTimeLine(wassr, twitter)
-            continue
-          elif input[0] in sendPostKey:
-            twitPost(unicode(' '.join(input[1:]), 'utf-8'))
-            continue
-          elif input[0] in getRepliesKey:
-            getReplies(wassr, twitter)
-            continue
-          elif input[0] in exitKey:
-            print 'bye ;-)'
-            break
+      if input[0] in getFriendsTimeLineKey: 
+        getFriensTimeLine(wassr, twitter)
+        continue
+      elif input[0] in sendPostKey:
+        twitPost(unicode(' '.join(input[1:]), 'utf-8'))
+        continue
+      elif input[0] in getRepliesKey:
+        getReplies(wassr, twitter)
+        continue
+      elif input[0] in exitKey:
+        print 'bye ;-)'
+        break
+      else:
+        twitPost(unicode('%s'%(all_input), 'utf-8'))
+        continue
     else: 
       getReplies(wassr, twitter)
